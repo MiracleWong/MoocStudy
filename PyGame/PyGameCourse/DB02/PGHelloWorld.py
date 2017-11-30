d134a98d70d02781605677c7f803cdbea6e59fad
MiracleWong <cfwr1991@126.com>
2017-11-30 10:21:01 +0800

    Add the helloworld sample

---
+++ PyGame/PyGameCourse/DB02/PGHelloWorld.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+import pygame,sys
+pygame.init()
+
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("PyGame游戏之旅")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    pygame.display.update()
